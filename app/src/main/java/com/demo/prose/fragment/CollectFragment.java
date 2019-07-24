3822614bea0baa6fc6efa749e0f120ee96a3a33d
xiaohuanghao <pp1118005@gmail.com>
2019-07-24 15:00:17 +0800

    co7.24 14:59决定把button从listview里拿出去

--- app/src/main/java/com/demo/prose/fragment/CollectFragment.java
+++ app/src/main/java/com/demo/prose/fragment/CollectFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -42,8 +43,7 @@ public class CollectFragment extends BaseFragment {
     @Override
     protected View initView() {
         view = inflate(mContext, R.layout.collect2, null);
-        img_show = (ImageView) view.findViewById(R.id.img_show);
-        btn_start = (Button) view.findViewById(R.id.btn_start);
+
         bindViews();
         listView=(ListView) view.findViewById(R.id.listView);
         //准备数据?
@@ -59,8 +59,8 @@ public class CollectFragment extends BaseFragment {
 
     private void bindViews() {
         //点击监听,拍摄
-       /* img_show = (ImageView) view.findViewById(R.id.img_show);
-        btn_start = (Button) view.findViewById(R.id.btn_start);*/
+        img_show = (ImageView) view.findViewById(R.id.img_show);
+        btn_start = (Button) view.findViewById(R.id.btn_start);
         btn_start.setOnClickListener(new View.OnClickListener() {
 //在按钮点击事件出写上这些东西,这些实在sd卡创建图片文件的
 
@@ -99,6 +99,8 @@ public class CollectFragment extends BaseFragment {
         super.onCreate(savedInstanceState);
 
     }
+/*                                adapter                                         */
+
 
     class PAdapter extends BaseAdapter {
         //返回集合数据的数量
@@ -119,6 +121,8 @@ public class CollectFragment extends BaseFragment {
             return 0;
         }
         //返回指定下标所对应的item的view对象
+
+        public  void  add(ContactsContract.RawContacts.Data data){}
         @Override
         public View getView(int position, View convertView, ViewGroup viewGroup) {
             if (convertView == null) {
